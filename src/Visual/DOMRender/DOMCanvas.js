9a88e758fa64c01eddfab28a229bc41547d2c21a
Chris <chrisyboy53@gmail.com>
2017-01-24 21:35:01 +0000

    No need for commented code

--- src/Visual/DOMRender/DOMCanvas.js
+++ src/Visual/DOMRender/DOMCanvas.js
@@ -2,8 +2,6 @@ var Visual = Visual || {};
 
 Visual.DOMRender = Visual.DOMRender || {};
 
-// Visual.DOMRender.DOMCanvas = Visual.DOMRender.DOMCanvas || {};
-
 (function(domRender) {
 
     var canvas = {};
